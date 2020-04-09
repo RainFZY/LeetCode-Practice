f65704a1e0aacdb13880ec539487e38a1ba1d755
RainFZY <201700121022@mail.sdu.edu.cn>
2020-04-09 23:17:53 +0800

    4.9

--- 53.最大子序和.py
+++ 53.最大子序和.py
@@ -17,21 +17,33 @@
 #         return max_sum
 
 # 法二：动态规划，思路基本跟贪心类似
-# 动态规划的是首先对数组进行遍历，当前最大连续子序列和为 sum，结果为 ans
+# 对数组进行遍历，对于每一个位置，若它之前的sum为负，说明还不如不要之前只要现在位置的来的大，就抛弃之前的
+# sum：当前位置之前的最大连续子序列和
+# ans：历史最大连续子序列和
 # 如果 sum > 0，则说明 sum 对结果有增益效果，则 sum 保留并加上当前遍历数字
 # 如果 sum <= 0，则说明 sum 对结果无增益效果，需要舍弃，则 sum 直接更新为当前遍历数字
 # 每次比较 sum 和 ans的大小，将最大值置为ans，遍历结束返回结果
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         sum = nums[0]
+#         ans = nums[0]
+#         for i in range(1, len(nums)):
+#             if sum > 0:
+#                 sum += nums[i]
+#             else:
+#                 sum = nums[i]
+#             ans = max(ans, sum)
+#         return ans
+
+# 法二变式
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        sum = nums[0]
-        ans = nums[0]
+        max_sum = nums[0]
         for i in range(1, len(nums)):
-            if sum > 0:
-                sum += nums[i]
-            else:
-                sum = nums[i]
-            ans = max(ans, sum)
-        return ans
+            if nums[i - 1] > 0:
+                nums[i] += nums[i - 1]
+            max_sum = max(max_sum, nums[i])
+        return max_sum
 
 
 # @lc code=end
