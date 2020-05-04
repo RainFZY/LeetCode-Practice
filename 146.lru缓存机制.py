956387da911c37c1b127b1e3106a13da85d96389
RainFZY <201700121022@mail.sdu.edu.cn>
2020-05-04 16:15:32 +0800

    5.4

--- 146.lru缓存机制.py
+++ 146.lru缓存机制.py
@@ -5,25 +5,27 @@
 #
 
 # @lc code=start
+# 有序字典，如果不用有序字典的话得手写双向链表实现
 from collections import OrderedDict
-class LRUCache(OrderedDict):
+class LRUCache:
 
     def __init__(self, capacity):
         self.capacity = capacity
+        self.dict = OrderedDict()
 
     def get(self, key):
-        if key not in self:
+        if key not in self.dict:
             return - 1
         
-        self.move_to_end(key)
-        return self[key]
+        self.dict.move_to_end(key)
+        return self.dict[key]
 
     def put(self, key, value):
-        if key in self:
-            self.move_to_end(key)
-        self[key] = value
-        if len(self) > self.capacity:
-            self.popitem(last = False)
+        if key in self.dict:
+            self.dict.move_to_end(key)
+        self.dict[key] = value
+        if len(self.dict) > self.capacity:
+            self.dict.popitem(last = False)
 
 # LRUCache 对象会以如下语句构造和调用:
 # obj = LRUCache(capacity)
