8200000fbbf952fbe9e7f7bd180aedd9fb9f5339
RainFZY <201700121022@mail.sdu.edu.cn>
2020-05-15 11:06:10 +0800

    5.15

---
+++ 125.验证回文串.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode.cn id=125 lang=python3
+#
+# [125] 验证回文串
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ""
+        for char in s:
+            # 字母
+            if char.isalpha():
+                res += char.lower()
+            # 数字
+            if char.isdigit():
+                res += char
+        # if ''.join(reversed(res)) == res
+        return True if res[::-1] == res else False
+
+# 双指针
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        while left < right:
+            # isalnum: num + alpha
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+# @lc code=end
+
---
+++ 680.验证回文字符串-ⅱ.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=680 lang=python3
+#
+# [680] 验证回文字符串 Ⅱ
+#
+
+# @lc code=start
+# 双指针
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                # 去掉右边的，去掉左边的
+                case1, case2 = s[left: right], s[left + 1: right + 1]
+                return case1[::-1] == case1 or case2[::-1] == case2
+            left += 1
+            right -= 1
+        return True
+# @lc code=end
+
