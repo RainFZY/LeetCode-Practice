5672e724e2372ee96a86317ff35a7e258d14b983
RainFZY <201700121022@mail.sdu.edu.cn>
2020-05-19 09:59:20 +0800

    5.19

---
+++ 115.不同的子序列.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=115 lang=python3
+#
+# [115] 不同的子序列
+#
+
+# @lc code=start
+# 二维DP，较难懂，https://leetcode-cn.com/problems/distinct-subsequences/solution/dong-tai-gui-hua-by-powcai-5/
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n1, n2 = len(s), len(t)
+        dp = [[0] * (n1 + 1) for _ in range(n2 + 1)]
+        for j in range(n1 + 1):
+            dp[0][j] = 1
+        for i in range(1, n2 + 1):
+            for j in range(1, n1 + 1):
+                if t[i - 1] == s[j - 1]:
+                    # 核心行
+                    # t的最后一个字符一定是匹配s的最后一个字符，所以同时删除最后一个 + 
+                    # t的最后一个字符一定不是对应s的最后一个字符，可以匹配s前面的，所以只删s的最后一个
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]
+                else:
+                    dp[i][j] = dp[i][j - 1]
+        return dp[-1][-1]
+# @lc code=end
+
