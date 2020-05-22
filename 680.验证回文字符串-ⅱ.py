dfb0adb6580ba1c34ab0ecefad13aa4e37e3a972
RainFZY <201700121022@mail.sdu.edu.cn>
2020-05-22 11:02:42 +0800

    5.22

--- 680.验证回文字符串-ⅱ.py
+++ 680.验证回文字符串-ⅱ.py
@@ -13,6 +13,7 @@ class Solution:
             if s[left] != s[right]:
                 # 去掉右边的，去掉左边的
                 case1, case2 = s[left: right], s[left + 1: right + 1]
+                # 最多删除一个字符，必须return
                 return case1[::-1] == case1 or case2[::-1] == case2
             left += 1
             right -= 1
