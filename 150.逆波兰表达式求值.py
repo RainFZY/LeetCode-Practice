e7e6c5a76df78f1568924069d44b11012610e943
RainFZY <201700121022@mail.sdu.edu.cn>
2020-06-03 11:52:29 +0800

    6.3

---
+++ 150.逆波兰表达式求值.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=150 lang=python3
+#
+# [150] 逆波兰表达式求值
+#
+
+# @lc code=start
+# 栈
+# 遇到数字压入栈, 遇到操作符, 弹出栈顶两个元素操作
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        plus = lambda a, b: b + a
+        sub = lambda a, b: b - a
+        mul = lambda a, b: b * a
+        # 整数除法只保留整数部分, //是向下取整，在负数时错误
+        div = lambda a, b: int(b / a)
+        opt = {
+            "+": plus,
+            "-": sub,
+            "*": mul,
+            "/": div
+        }
+        for t in tokens:
+            if t in opt:
+                stack.append(opt[t](stack.pop(), stack.pop()))
+            else:
+                stack.append(int(t))
+        return stack.pop()
+# @lc code=end
+
