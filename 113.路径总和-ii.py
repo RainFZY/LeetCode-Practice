0d645cf1a3121412197245795a74c7f7d80f1af5
RainFZY <201700121022@mail.sdu.edu.cn>
2020-06-11 10:20:40 +0800

    6.11

---
+++ 113.路径总和-ii.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=113 lang=python3
+#
+# [113] 路径总和 II
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# 递归
+# 经典递归模板
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        res = []
+        def recursion(root, sum, temp):
+            if not root:
+                return 
+            if not root.left and not root.right and sum - root.val == 0:
+                temp += [root.val]
+                res.append(temp)
+            recursion(root.left, sum - root.val, temp + [root.val])
+            recursion(root.right, sum - root.val, temp + [root.val])
+        recursion(root, sum, [])
+        return res
+
+
+# @lc code=end
+
