7aa80c0033fbfbbc54ece2b8e9ad3ccdf8bc942b
RainFZY <201700121022@mail.sdu.edu.cn>
2020-09-08 17:27:41 +0800

    2020.9.8

---
+++ 100.相同的树.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=100 lang=python3
+#
+# [100] 相同的树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 递归
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p == None and q == None:
+            return True
+        if p == None or q == None:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and \
+            self.isSameTree(p.right, q.right)
+# @lc code=end
+
